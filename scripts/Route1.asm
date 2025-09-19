91df109e27064440a2662e675382b9e3a317d20b
UberArsenal <dannymail14@gmail.com>
2025-09-19 19:22:10 -0400

    Squashed commit of the following:
    commit a314975fad49de8fcffd3beabc0b6752baf4a2d5
    Author: UberArsenal <dannymail14@gmail.com>
    Date:   Fri Sep 19 19:21:28 2025 -0400
    
        Fix Route 1 text order for Oak

--- scripts/Route1.asm
+++ scripts/Route1.asm
@@ -82,8 +82,8 @@ Route1_TextPointers:
         def_text_pointers
         dw_const Route1Youngster1Text, TEXT_ROUTE1_YOUNGSTER1
         dw_const Route1Youngster2Text, TEXT_ROUTE1_YOUNGSTER2
-        dw_const Route1SignText,       TEXT_ROUTE1_SIGN
         dw_const Route1OakText,        TEXT_ROUTE1_OAK
+        dw_const Route1SignText,       TEXT_ROUTE1_SIGN
 
 Route1Youngster1Text:
         text_asm
