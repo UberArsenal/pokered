8427f285805703e0d29eae48508e50a8fce79960
UberArsenal <dannymail14@gmail.com>
2025-09-19 14:25:36 -0400

    Refactor map object helpers into ROMX bank

--- home/map_objects.asm
+++ home/map_objects.asm
@@ -3,28 +3,7 @@
 ; b = player Y
 ; c = player X
 DecodeArrowMovementRLE::
-	ld a, [hli]
-	cp $ff
-	ret z ; no match in the list
-	cp b
-	jr nz, .nextArrowMovementTileEntry1
-	ld a, [hli]
-	cp c
-	jr nz, .nextArrowMovementTileEntry2
-	ld a, [hli]
-	ld d, [hl]
-	ld e, a
-	ld hl, wSimulatedJoypadStatesEnd
-	call DecodeRLEList
-	dec a
-	ld [wSimulatedJoypadStatesIndex], a
-	ret
-.nextArrowMovementTileEntry1
-	inc hl
-.nextArrowMovementTileEntry2
-	inc hl
-	inc hl
-	jr DecodeArrowMovementRLE
+        farjp DecodeArrowMovementRLE_
 
 TextScript_ItemStoragePC::
 	call SaveScreenTilesToBuffer2
@@ -177,70 +156,20 @@ _GetPointerWithinSpriteStateData:
 ; de: input list
 ; hl: output list
 DecodeRLEList::
-	xor a
-	ld [wRLEByteCount], a     ; count written bytes here
-.listLoop
-	ld a, [de]
-	cp $ff
-	jr z, .endOfList
-	ldh [hRLEByteValue], a ; store byte value to be written
-	inc de
-	ld a, [de]
-	ld b, $0
-	ld c, a                      ; number of bytes to be written
-	ld a, [wRLEByteCount]
-	add c
-	ld [wRLEByteCount], a     ; update total number of written bytes
-	ldh a, [hRLEByteValue]
-	call FillMemory              ; write a c-times to output
-	inc de
-	jr .listLoop
-.endOfList
-	ld a, $ff
-	ld [hl], a                   ; write final $ff
-	ld a, [wRLEByteCount]
-	inc a                        ; include sentinel in counting
-	ret
+        farjp DecodeRLEList_
 
 ; sets movement byte 1 for sprite [hSpriteIndex] to $FE and byte 2 to [hSpriteMovementByte2]
 SetSpriteMovementBytesToFE::
-	push hl
-	call GetSpriteMovementByte1Pointer
-	ld [hl], $fe
-	call GetSpriteMovementByte2Pointer
-	ldh a, [hSpriteMovementByte2]
-	ld [hl], a
-	pop hl
-	ret
+        farjp SetSpriteMovementBytesToFE_
 
 ; sets both movement bytes for sprite [hSpriteIndex] to $FF
 SetSpriteMovementBytesToFF::
-	push hl
-	call GetSpriteMovementByte1Pointer
-	ld [hl], STAY
-	call GetSpriteMovementByte2Pointer
-	ld [hl], NONE
-	pop hl
-	ret
+        farjp SetSpriteMovementBytesToFF_
 
 ; returns the sprite movement byte 1 pointer for sprite [hSpriteIndex] in hl
 GetSpriteMovementByte1Pointer::
-	ld h, HIGH(wSpriteStateData2)
-	ldh a, [hSpriteIndex]
-	swap a
-	add 6
-	ld l, a
-	ret
+        farjp GetSpriteMovementByte1Pointer_
 
 ; returns the sprite movement byte 2 pointer for sprite [hSpriteIndex] in hl
 GetSpriteMovementByte2Pointer::
-	push de
-	ld hl, wMapSpriteData
-	ldh a, [hSpriteIndex]
-	dec a
-	add a
-	ld d, 0
-	ld e, a
-	add hl, de
-	pop de
-	ret
+        farjp GetSpriteMovementByte2Pointer_
